44e6518737817b470348e70b91fa46d9573a0a93
Julien SANCHEZ <chadow.video@gmail.com>
2023-02-06 00:48:58 +0100

    here

--- Assets/Scripts/Astronaute/PlayerHealthBar.cs
+++ Assets/Scripts/Astronaute/PlayerHealthBar.cs
@@ -6,14 +6,27 @@ using UnityEngine.UI;
 public class PlayerHealthBar : MonoBehaviour
 {
    
-   public int health = 5;
-   [SerializeField] Text healthScore;
+   public int health = 3;
+   public int maxHealth = 3;
+   public Image healthBar;
 
+   void Start() {
+      healthBar.fillAmount = 1;
+   
+   }
    void Update(){
       if(health == 0){
+         healthBar.fillAmount = 0;
+         Time.timeScale = 0; // for debug
          Application.Quit();	
       }
-      healthScore.text = "Vie : " + health;
+      if(health == 1){
+         healthBar.fillAmount = 0.33f;
+      }
+      if(health == 2){
+         healthBar.fillAmount = 0.66f;
+      }
+    
    }
 
 }
--- Assets/Scripts/Monster/AttackPlayer.cs
+++ Assets/Scripts/Monster/AttackPlayer.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class AttackPlayer : MonoBehaviour
 {
 	public AudioSource soundEffect;
+	private float shakeDuration = 0.5f;
+	private float shakeAmount = 0.1f;
+	private Vector3 originalPosition;
 
 	private void OnTriggerEnter2D(Collider2D other){
  		if(other.tag == "Player"){
@@ -13,7 +16,26 @@ public class AttackPlayer : MonoBehaviour
 				healthBar.health = healthBar.health -1;
 				print(other.tag + " took damage " + " currentLife is " + healthBar.health);
 				soundEffect.Play();
+				StartCoroutine(Shake(other.gameObject));
 			}
 		}	
+	}
+
+	private IEnumerator Shake(GameObject obj) {
+   		float elapsed = 0.0f;
+   		originalPosition = obj.transform.position;
+
+   		while (elapsed < shakeDuration) {
+      		float x = Random.Range(-0.1f, 0.1f) * shakeAmount;
+      		float y = Random.Range(-0.1f, 0.1f) * shakeAmount;
+
+      	obj.transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+     	elapsed += Time.deltaTime;
+
+      	yield return null;
+   		}
+
+   		obj.transform.position = originalPosition;
 	}
 }
